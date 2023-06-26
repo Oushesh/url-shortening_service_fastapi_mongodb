d110ea8a375f584e0549e1e43608f22f4cc46863
oushesh <oushesh@gmail.com>
2023-06-26 18:17:41 +0400

    added tests for pytest test examples

--- test/test_index.py
+++ test/test_index.py
@@ -41,6 +41,7 @@ async def test_decode_url():
 ## Pairwise testing for N examples and see 
 ## what and why thing fails.
 
+"""
 @pytest.mark.asyncio
 async def test_encode_decode_recover():
     encoded_URL = "https://url-shortening-service-ol9j.vercel.app/encode_url"
@@ -60,11 +61,89 @@ async def test_encode_decode_recover():
     original_url = decoded_response.json()["long_url"]
     #Check that the decoded reponse is the original url.
     assert (input_url==original_url)
+"""
 
-  
 
-if __name__ == "__main__":
-    test_encode_decode_recover()
+class TestURLShortener:
+
+    encoded_URL = "https://url-shortening-service-ol9j.vercel.app/encode_url"
+    decoded_URL = "https://url-shortening-service-ol9j.vercel.app/decode_url"
+
+    @pytest.mark.asyncio
+    async def test_encode_decode_recover(self):
+        input_urls = [
+            "https://www.google.com",
+            "https://www.example.com",
+            "https://www.github.com",
+            "https://www.openai.com"
+        ]
+        errors = []
+        
+        for url in input_urls:
+            try:
+                encoded_params = {"url_input": url}
+                encoded_response = requests.get(self.encoded_URL, params=encoded_params)
+                assert encoded_response.status_code == 200
+
+                data = encoded_response.json()
+                encoded_data = data["short_url"]
+
+                decoded_params = {"short_url": encoded_data}
+                decoded_response = requests.get(self.decoded_URL, params=decoded_params)
+
+                original_url = decoded_response.json()["long_url"]
+                assert url == original_url
+
+            except AssertionError:
+                errors.append(f"Assertion failed for URL: {url}")
+
+        if errors:
+            pytest.fail("\n".join(errors))
+
+
+import pytest
+import requests
+
+class TestURLShortenerFail:
+
+    encoded_URL = "https://url-shortening-service-ol9j.vercel.app/encode_url"
+    decoded_URL = "https://url-shortening-service-ol9j.vercel.app/decode_url"
+
+    @pytest.mark.asyncio
+    async def test_encode_decode_recover(self):
+        input_urls = [
+            "https://www.google.com",
+            "https://www.example.com",
+            "https://www.github.com",
+            "https://www.openai.com",
+            "https://www.invalidurl.com",  # Invalid URL intentionally added
+            "https://www.example.com/page?param1=value1&param2=value2&param3=value3",
+            "https://www.google.com/search?q=python+programming&num=10",
+            "https://www.github.com/repo/issues?state=open&labels=bug",
+            "https://www.openai.com/blog/the-future-of-artificial-intelligence",
+        ]
+        errors = []
+        
+        for url in input_urls:
+            try:
+                encoded_params = {"url_input": url}
+                encoded_response = requests.get(self.encoded_URL, params=encoded_params)
+                assert encoded_response.status_code == 200
+
+                data = encoded_response.json()
+                encoded_data = data["short_url"]
+
+                decoded_params = {"short_url": encoded_data}
+                decoded_response = requests.get(self.decoded_URL, params=decoded_params)
+
+                original_url = decoded_response.json()["long_url"]
+                assert url == original_url+"url"
+
+            except AssertionError:
+                errors.append(f"Assertion failed for URL: {url}")
+
+        if errors:
+            pytest.fail("\n".join(errors))
 
 
 
